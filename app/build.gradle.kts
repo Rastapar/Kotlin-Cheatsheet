5d6b85c28e4cb538bdea8253d131a9d8671a9c9c
rastapar <romancher799@gmail.com>
2023-09-28 21:29:06 +0200

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,14 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.compose.ui:ui-android:1.5.1")
+    implementation("androidx.compose.material:material-android:1.5.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
 }
--- app/src/main/java/com/example/kotlin_tutorial/flows/1.Flows.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kotlin_tutorial.flows
-
-/*
-    The Flows are all about being notified about changes in your code and sending them through a
-    pipeline that potentially modifies them.
-    A Flow at the end is a coroutine that can emit multiple values over a period of time.
- */
